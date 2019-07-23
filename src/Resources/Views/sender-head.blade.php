bb128e67483da01449d762c490f454b8a535c3e9
Nate Nolting <me@natenolting.com>
2019-07-23 16:07:33 -0500

    Added title string

--- src/Resources/Views/sender-head.blade.php
+++ src/Resources/Views/sender-head.blade.php
@@ -1,3 +1,4 @@
+<p style="font-size: 150%; font-weight: 700;">Thanks for filling out the {!! title_case(str_replace(['-','_'], [' ', ' '], $data['formName'])) !!} form!</p>
 <p>A Message was sent to {{ $data['recipient'] }} {{ isset($data['name']) && strlen($data['name']) > 0 ? '( '.$data['name'] . ' )' : ''  }} at {{ $data['time'] }}</p>
 <p>Message that was sent below:</p>
 @if( isset($data['body']))
